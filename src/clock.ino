14bb3b8c30b8b8be26190f09bbd103193469a8d8
Shahriar <shahriar.yazdipour@outlook.com>
2020-07-23 11:11:53 +0200

    Update clock.ino

--- src/clock.ino
+++ src/clock.ino
@@ -1,14 +1,6 @@
-// This clock does not (yet) synchronise with any time server and does not use a real clock module to stay exactly on time (yet).
-// It just runs a basic seconds count with a 1000 millisecond delay in the sketch. LEDs are connected to PWM 3 to 19 with a resistor each.
-// PWM pins 14 to 19 are the Analog pins 0 to 5.
-// pins 2 - 13 are the regular digital pwm pins.
 int ledPinsSec[] = {2, 3, 4, 5, 6, 7};
 int ledPinsMin[] = {8, 9, 10, 11, 12, 13};
-
-// pins 14 - 20 are the analog pins 0 - 5 used as digital pwm pins  
 int ledPinsHr[] = {14, 15, 16, 17, 18, 19};
-
-// set Start time here
 int countS = 0;   // Seconds
 int countM = 13;  // Minutes
 int countH = 23;  // Hours
@@ -35,8 +27,6 @@ void setup(void)
   }
 }
 
-// ----- Main Routine -------------------------------------------------
-
 void loop(void)
 {
   countS = (countS + 1);
@@ -61,11 +51,8 @@ void loop(void)
   dispBinaryMin(countM);
   dispBinaryHr(countH);
 
-  delay(1000);   // 1000 milliseconds = approx. 1 second delay
-  // ADJUST for fast or slow running clock here, in milliseconds.
+  delay(1000);
 }
-
-// ----- Subroutines ---------------------------------------------------
 
 void dispBinarySec(byte nSec)
 {
